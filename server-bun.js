f20438027ffa3755748d64c4f8eda100d45cf2f3
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-12 13:29:39 +0530

    create http server with bun

---
+++ server-bun.js
@@ -0,0 +1,18 @@
+import { serve } from "bun";
+
+serve({
+  fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      return new Response("Hello Coffee", { status: 200 });
+    } else if (url.pathname === "/ice-tea") {
+      return new Response("Hello its Ice Tea", { status: 200 });
+    } else {
+      return new Response("404 Not Found", { status: 404 });
+    }
+  },
+  port: 3000,
+  hostname: "127.0.0.1",
+});
+
+// not a good dev experience.
